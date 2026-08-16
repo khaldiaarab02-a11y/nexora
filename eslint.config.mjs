ad1b88b21a5666fa22c8004a2fb93882f43436ba
khaldiaarab02-a11y <khaldiaarab02@gmail.com>
2026-08-16 16:13:39 +0100

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,1 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
-
-export default defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  globalIgnores([
-    ".next/**",
-    "node_modules/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default [];
