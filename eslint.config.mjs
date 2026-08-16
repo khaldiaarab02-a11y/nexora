d45eb962e9a5eb3349b613c34445120a55168ab1
khaldiaarab02-a11y <khaldiaarab02@gmail.com>
2026-08-16 15:57:10 +0100

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript";
+import nextTs from "eslint-config-next/typescript.js";
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
   globalIgnores([
     ".next/**",
+    "node_modules/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
   ]),
 ]);
-
-export default eslintConfig;
